a40653de089fd9456181d69e0946945f3f9e9c69
William Crépault <william.crepault@gmail.com>
2024-10-09 22:01:32 -0400

    changé d'idée, suppression de la commande qui instale mysqli via un Dockerfile et changement du code php de index.php pour afficher les ip conteneur et extérieur de la vm

--- html/index.php
+++ html/index.php
@@ -1,14 +1,13 @@
 <h1>Je te test!</h1>
-<h4>Tentative de connexion à MySQL depuis PHP...</h4>
+<h4>Tentative d'affichage des ip du conteneur PHP-FPM et de la VM!</h4>
 
 <?php
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+// Afficher l'adresse IP du conteneur
+echo "L'adresse IP du conteneur est : " . $_SERVER['SERVER_ADDR'] . "<br>";
 
-if ($conn->connect_error) {
-    die ("La connection a échoué: " . $conn->connect_error);
-}
-echo "Connexion réussi à MySQL!";
-?
+// Afficher l'adresse IP externe de la VM
+$ipExterne = file_get_contents('http://ipecho.net/plain');
+echo "L'adresse IP externe de la VM est : " . $ipExterne;
+?>
+
+
